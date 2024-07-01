8d20d4bb61c13cfe521b0f23caeb2d3c86940dd8
Abubakar Adam Sani <Athernee@gmail.com>
2024-07-01 15:44:43 +0100

    Initiate simple routes setup

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Task Manager App')
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import routes from './routes/tasks-route.js';
+
+const app = express();
+
+// Middleware setup
+app.use(express.json());
+
+//mount routes
+app.use('/api/v1', routes);
+
+app.get('/', (req, res) => {
+  return res.status(200).send('Welcome to The TaskManager App.');
+});
+
+try {
+  const port = process.env.PORT || 3000
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+  });
+} catch (err) {
+  console.log('Error Encountered =>', err);
+}
